2d064a3ea3038314a7b661848376844dd8c4b26e
Asmir H <asmirh@outlook.com>
2021-10-23 13:32:30 +0200

    prop size and const colors

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF4059d6);
+const kPrimaryDarkColor = Color(0xFF3386fd);
+const kPrimaryLight = Color(0xFF397fca);
+const kBackgroundColor = Color(0xfffefefe);
+const kPrimaryLightColor = Color(0xFFf5f5fa);
+const kGreenDarkColor = Color(0xff11b44c);
+const kGreenLightColor = Color(0xffc4f5d4);
+const kPurpleDarkColor = Color(0xff715cce);
+const kPurpleLightColor = Color(0xffe8e4fc);
+const kBlueDarkColor = Color(0xff4059d6);
+const kBlueLightColor = Color(0xffcce2f8);
+const kRedDarkColor = Color(0xffd43e3e);
+const kRedLightColor = Color(0xfffed8d8);
+const kYellowDarkColor = Color(0xffffd100);
+const kYellowLightColor = Color(0xfffff9e7);
+
